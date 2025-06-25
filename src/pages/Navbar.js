0a7296cdbd63a130d8e72e6dfe4dbcfc25e11bf0
Mohd Amjad Ali <2003amjadali@gmail.com>
2025-06-26 03:21:31 +0530

    Update Navbar.js

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -7,7 +7,7 @@ import './Navbar.css';
 const pages = [
   { name: 'Home', path: '/' },
   { name: 'CodePad', path: '/codepad' },
-  { name: 'Tutorials', path: '/learn' },
+  { name: 'Tutorials', path: '/tutorial' },
   { name: 'Resources', path: '/resources' },
   { name: 'Interview Prep', path: '/interview-prep' },
   { name: 'Community', path: '/community' },
